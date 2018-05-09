32d8b7be4e3c2af378c997b532bac286b82afd20
tkhrmd <maeda1991@gmail.com>
2018-05-09 11:16:29 +0900

    暫定対応: DOMのレンダリングを待ってスクリプトを注入

--- content_scripts.js
+++ content_scripts.js
@@ -1,4 +1,17 @@
-const el = document.createElement('script');
-el.setAttribute('type', 'text/javascript');
-el.setAttribute('src', chrome.extension.getURL('dist/main.js'));
-document.body.appendChild(el);
+const wait = () => {
+  setTimeout(() => {
+    if (document.getElementById('_chatText')) {
+      inject();
+      return;
+    }
+    wait();
+  }, 0);
+}
+wait();
+
+const inject = () => {
+  const el = document.createElement('script');
+  el.setAttribute('type', 'text/javascript');
+  el.setAttribute('src', chrome.extension.getURL('dist/main.js'));
+  document.body.appendChild(el);
+};
